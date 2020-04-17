d5cc6c7483b17f8bdac185d1720e42e8a1a19dbd
vishnu <srivishnu995@gmail.com>
2020-04-17 14:09:08 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/signup';
+import Signin from './Components/singin'
 
 function App() {
   return (
     <div className="App">
   
         <Signup />
-     
+        <Signin/>
     </div>
   );
 }
