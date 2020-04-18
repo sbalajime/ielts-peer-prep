8f4e44eea6f62fa4d94eac4cdd22f6bf02374dbc
vishnu <srivishnu995@gmail.com>
2020-04-18 16:32:52 +0530

    modified frontend

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/signup';
 import Signin from './Components/singin'
+import {Link , BrowserRouter as Router , Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
+    <Router>
+     <header id="Pageheader">
+        <h1>Page Heading</h1>
+      </header>  
     <div className="App">
-  
-        <Signup />
-        <Signin/>
+        <Switch>
+        <Route path = "/signup"  > 
+        <Signup/>
+         </Route> 
+         <Route path ="/" >
+          <Signin/>
+         </Route>
+        </Switch>
+        
     </div>
+    </Router>
   );
 }
 
---
+++ src/Components/homeimg.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import home from  './pics/home.svg'
+
+
+function Homeimg(){
+
+return(
+<img className ="homeImg" src= {home}  alt="pic" />
+)
+
+}
+
+
+export default Homeimg;
