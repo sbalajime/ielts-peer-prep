4fef8e98346d566035d848ec79cc8b82bb3f0040
Balaji S <sbalaji@protonmail.com>
2020-04-21 08:00:00 +0530

    Enhanced review page styles for responsiveness

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -7,8 +7,19 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = (theme) => ({
+    reviewSection: {
+        '& > *': {
+            margin: theme.spacing(2)
+        }
+    },
+    slideLabel: {
+        marginRight: theme.spacing(4)
+    },
     root: {
         width: '100%',
         margin: theme.spacing(3)
@@ -24,6 +35,19 @@ const useStyles = (theme) => ({
     pos: {
         marginBottom: 12,
     },
+    divider: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2)
+    },
+    sliderRow: {
+        padding: theme.spacing(2)
+    },
+    comments: {
+        padding: theme.spacing(9),
+        [theme.breakpoints.down('sm')]: {
+            padding: theme.spacing(2)
+        },
+    }
 });
 
 function valueLabelFormat(value) {
@@ -36,6 +60,12 @@ function valueLabelFormat(value) {
 
 
 class Review extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: ''
+        }
+    }
     render() {
         const { classes } = this.props;
         const bull = <span className={classes.bullet}>•</span>;
@@ -46,13 +76,14 @@ class Review extends Component {
                         <Typography variant="h5" component="h2" gutterBottom>
                             Question
                         </Typography>
-                        <Typography variant="body2" component="p">
+                        <Typography variant="body2" component="p" gutterBottom>
                             Doing an enjoyable activity with a child can develop better skills and more creativity than reading. To what extent do you agree? Use reasons and specific examples to explain your answer.
                         </Typography>
-                        <Typography variant="h5" component="h2">
+                        <Divider className={classes.divider} />
+                        <Typography variant="h5" component="h2" gutterBottom>
                             Answer
                         </Typography>
-                        <Typography variant="body2" component="p">
+                        <Typography variant="body2" component="p" gutterBottom>
                             Parents throughout the world place spend time reading with their offspring to prepare them for school where their literacy skills are further developed; however, recent research suggests that focusing on reading at an early age can be detrimental, and participating in fun activities would be far more beneficial. I am a strong advocate of this approach, and the benefits of it will be covered in this essay.
                             A fundamental reason for this is that there is no biological age for reading, and pushing infants to acquire this skill before they are ready could have repercussions. For example, in the UK, many boys are reluctant readers, possibly because of being forced to read, and this turned them off reading. By focusing on other activities and developing other skills such as creativity and imagination, when they are ready to read, they usually acquire this skill rapidly. In addition, the importance of encouraging creativity and developing a child’s imagination must be acknowledged. Through play, youngsters develop social and cognitive skills, for example, they are more likely to learn vocabulary through context rather than learning it from a book.
 
@@ -61,20 +92,97 @@ class Review extends Component {
                             Despite being a supporter of this non-reading approach, I strongly recommend incorporating bedtime stories into a child’s daily routine. However, reading as a regular daytime activity should be swapped for something which allows the child to develop other skills.
                         </Typography>
                     </CardContent>
+                    <Divider className={classes.divider} />
                     <CardActions>
-                        <Slider
-                            defaultValue={1}
-                            getAriaValueText={valueLabelFormat}
-                            aria-labelledby="discrete-slider-always"
-                            step={1}
-                            min={1}
-                            max={9}
-                            valueLabelDisplay="on"
-                        />
-                        <Button size="small">Learn More</Button>
+                        <Grid container spacing={5}>
+                            <Grid item lg={6} sm={12} xs={12}>
+                                <Grid container spacing={1} className={classes.sliderRow}>
+                                    <Grid item lg={6} sm={6} xs={6} ><Typography variant="body2" className={classes.slideLabel}>
+                                        Task Achievement
+                                </Typography>
+                                    </Grid>
+                                    <Grid item lg={6} sm={6} xs={6} >
+                                        <Slider
+                                            defaultValue={1}
+                                            getAriaValueText={valueLabelFormat}
+                                            aria-labelledby="discrete-slider-always"
+                                            step={1}
+                                            min={1}
+                                            max={9}
+                                            valueLabelDisplay="on"
+                                        /></Grid>
+                                </Grid>
+                                <Grid container spacing={1} className={classes.sliderRow}>
+                                    <Grid item lg={6} sm={6} xs={6} ><Typography variant="body2" className={classes.slideLabel}>
+                                        Coherence and Cohesion
+                                </Typography>
+                                    </Grid>
+                                    <Grid item lg={6} lg={6} sm={6} xs={6} >
+                                        <Slider
+                                            defaultValue={1}
+                                            getAriaValueText={valueLabelFormat}
+                                            aria-labelledby="discrete-slider-always"
+                                            step={1}
+                                            min={1}
+                                            max={9}
+                                            valueLabelDisplay="on"
+                                        /></Grid>
+                                </Grid>
+                                <Grid container spacing={1} className={classes.sliderRow}>
+                                    <Grid item lg={6} sm={6} xs={6} ><Typography variant="body2" className={classes.slideLabel}>
+                                        Lexical Resource
+                                </Typography>
+                                    </Grid>
+                                    <Grid item lg={6} lg={6} sm={6} xs={6} >
+                                        <Slider
+                                            defaultValue={1}
+                                            getAriaValueText={valueLabelFormat}
+                                            aria-labelledby="discrete-slider-always"
+                                            step={1}
+                                            min={1}
+                                            max={9}
+                                            valueLabelDisplay="on"
+                                        /></Grid>
+                                </Grid>
+                                <Grid container spacing={1} className={classes.sliderRow}>
+                                    <Grid item lg={6} lg={6} sm={6} xs={6} ><Typography variant="body2" className={classes.slideLabel}>
+                                        Grammatical Range and Accuracy
+                                </Typography>
+                                    </Grid>
+                                    <Grid item lg={6} lg={6} sm={6} xs={6} >
+                                        <Slider
+                                            defaultValue={1}
+                                            getAriaValueText={valueLabelFormat}
+                                            aria-labelledby="discrete-slider-always"
+                                            step={1}
+                                            min={1}
+                                            max={9}
+                                            valueLabelDisplay="on"
+                                        /></Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid item lg={6} sm={12} xs={12}>
+                                <Box className={classes.comments}><TextField
+                                    id="outlined-multiline-static"
+                                    label="Comments (*optional)"
+                                    multiline
+                                    rows={5}
+                                    value={this.state.comments}
+                                    onChange={(e) => this.handleChange('question', e.target.value)}
+                                    variant="outlined"
+                                    fullWidth={true}
+                                    inputProps={{ "data-gramm_editor": false, "data-gramm": false, spellCheck: false }}
+                                    margin=""
+                                /></Box>
+                            </Grid>
+                            <Grid item lg={12} sm={12} xs={12}>
+                                <div style={{ marginTop: 20, width: '100%', textAlign: 'right' }}><Button variant="contained" color="primary" classes={{ root: classes.button }} onClick={this.handleClick}>Submit Review</Button></div>
+                            </Grid>
+                        </Grid>
+
                     </CardActions>
                 </Card>
-            </Box>
+            </Box >
 
         )
     }
